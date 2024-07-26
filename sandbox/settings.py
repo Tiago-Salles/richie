088d3f4bae5bf804d59b4493ecfc4e6e91cbc51e
Tiago-Salles <tiagosalles20@gmail.com>
2024-07-26 09:54:59 +0000

    ✨(backend) add offer and price fields to courseRun

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -599,6 +599,25 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
         environ_prefix=None,
     )
 
+    RICHIE_DEFAULT_COURSE_RUN_OFFER = values.Value(
+        "free",
+        environ_name="RICHIE_DEFAULT_COURSE_RUN_OFFER",
+        environ_prefix=None,
+    )
+
+    RICHIE_DEFAULT_COURSE_RUN_CERTIFICATE_OFFER = values.Value(
+        "free",
+        environ_name="RICHIE_DEFAULT_COURSE_RUN_CERTIFICATE_OFFER",
+        environ_prefix=None,
+    )
+
+    # Course run price currency value that would be shown on course detail page
+    RICHIE_DEFAULT_COURSE_RUN_PRICE_CURRENCY = values.Value(
+        "EUR",
+        environ_name="RICHIE_DEFAULT_COURSE_RUN_PRICE_CURRENCY",
+        environ_prefix=None,
+    )
+
     @classmethod
     def _get_environment(cls):
         """Environment in which the application is launched."""
